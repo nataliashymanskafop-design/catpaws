0797d93c4a28349a8ac2983a0d8fd612ad4411ea
nataliashymanskafop-design <natalia.shymanska.fop@gmail.com>
2026-07-07 23:13:44 +0300

    Update generate_feed.py

--- scripts/generate_feed.py
+++ scripts/generate_feed.py
@@ -1,1 +1,11 @@
-print("Hello Mono Market")
+import requests
+
+print("Завантаження XML...")
+
+url = "https://catpaws.com.ua/content/export/f58d0310c9401a7213540d5b6d75420a.xml"
+
+r = requests.get(url, timeout=60)
+
+print("Статус:", r.status_code)
+
+print("Розмір:", len(r.text))
