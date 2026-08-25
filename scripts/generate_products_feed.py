da1afc44688e8ac7ab6bb805ea32117d82334871
nataliashymanskafop-design <natalia.shymanska.fop@gmail.com>
2026-08-25 23:36:19 +0300

    Update generate_products_feed.py

--- scripts/generate_products_feed.py
+++ scripts/generate_products_feed.py
@@ -3,7 +3,7 @@ import requests
 from lxml import etree
 
 
-XML_URL = "https://catpaws.com.ua/content/export/4533d74998115a860f812d46074665c9.xml"
+XML_URL = "https://catpaws.com.ua/content/export/3e9c244f28ee6d1e572f92646e76f6bb.xml"
 MIN_PRICE = 300
 
 def main():
